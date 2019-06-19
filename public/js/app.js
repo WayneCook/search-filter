97f667548905d73b101774a22167dc5443e1c077
unknown <waynedemetra@gmail.com>
2019-06-19 10:29:24 -0700

    fix bootstrap error

--- public/js/app.js
+++ public/js/app.js
@@ -113,7 +113,6 @@ var buildURL = __webpack_require__(/*! ./../helpers/buildURL */ "./node_modules/
 var parseHeaders = __webpack_require__(/*! ./../helpers/parseHeaders */ "./node_modules/axios/lib/helpers/parseHeaders.js");
 var isURLSameOrigin = __webpack_require__(/*! ./../helpers/isURLSameOrigin */ "./node_modules/axios/lib/helpers/isURLSameOrigin.js");
 var createError = __webpack_require__(/*! ../core/createError */ "./node_modules/axios/lib/core/createError.js");
-var btoa = (typeof window !== 'undefined' && window.btoa && window.btoa.bind(window)) || __webpack_require__(/*! ./../helpers/btoa */ "./node_modules/axios/lib/helpers/btoa.js");
 
 module.exports = function xhrAdapter(config) {
   return new Promise(function dispatchXhrRequest(resolve, reject) {
@@ -125,22 +124,6 @@ module.exports = function xhrAdapter(config) {
     }
 
     var request = new XMLHttpRequest();
-    var loadEvent = 'onreadystatechange';
-    var xDomain = false;
-
-    // For IE 8/9 CORS support
-    // Only supports POST and GET calls and doesn't returns the response headers.
-    // DON'T do this for testing b/c XMLHttpRequest is mocked, not XDomainRequest.
-    if ( true &&
-        typeof window !== 'undefined' &&
-        window.XDomainRequest && !('withCredentials' in request) &&
-        !isURLSameOrigin(config.url)) {
-      request = new window.XDomainRequest();
-      loadEvent = 'onload';
-      xDomain = true;
-      request.onprogress = function handleProgress() {};
-      request.ontimeout = function handleTimeout() {};
-    }
 
     // HTTP basic authentication
     if (config.auth) {
@@ -155,8 +138,8 @@ module.exports = function xhrAdapter(config) {
     request.timeout = config.timeout;
 
     // Listen for ready state
-    request[loadEvent] = function handleLoad() {
-      if (!request || (request.readyState !== 4 && !xDomain)) {
+    request.onreadystatechange = function handleLoad() {
+      if (!request || request.readyState !== 4) {
         return;
       }
 
@@ -173,9 +156,8 @@ module.exports = function xhrAdapter(config) {
       var responseData = !config.responseType || config.responseType === 'text' ? request.responseText : request.response;
       var response = {
         data: responseData,
-        // IE sends 1223 instead of 204 (https://github.com/axios/axios/issues/201)
-        status: request.status === 1223 ? 204 : request.status,
-        statusText: request.status === 1223 ? 'No Content' : request.statusText,
+        status: request.status,
+        statusText: request.statusText,
         headers: responseHeaders,
         config: config,
         request: request
@@ -984,54 +966,6 @@ module.exports = function bind(fn, thisArg) {
     return fn.apply(thisArg, args);
   };
 };
-
-
-/***/ }),
-
-/***/ "./node_modules/axios/lib/helpers/btoa.js":
-/*!************************************************!*\
-  !*** ./node_modules/axios/lib/helpers/btoa.js ***!
-  \************************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-// btoa polyfill for IE<10 courtesy https://github.com/davidchambers/Base64.js
-
-var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-
-function E() {
-  this.message = 'String contains an invalid character';
-}
-E.prototype = new Error;
-E.prototype.code = 5;
-E.prototype.name = 'InvalidCharacterError';
-
-function btoa(input) {
-  var str = String(input);
-  var output = '';
-  for (
-    // initialize result and counter
-    var block, charCode, idx = 0, map = chars;
-    // if the next str index does not exist:
-    //   change the mapping table to "="
-    //   check if d has no fractional digits
-    str.charAt(idx | 0) || (map = '=', idx % 1);
-    // "8 - idx % 1 * 8" generates the sequence 2, 4, 6, 8
-    output += map.charAt(63 & block >> 8 - idx % 1 * 8)
-  ) {
-    charCode = str.charCodeAt(idx += 3 / 4);
-    if (charCode > 0xFF) {
-      throw new E();
-    }
-    block = block << 8 | charCode;
-  }
-  return output;
-}
-
-module.exports = btoa;
 
 
 /***/ }),
@@ -6779,7 +6713,7 @@ exports = module.exports = __webpack_require__(/*! ../../../../node_modules/css-
 
 
 // module
-exports.push([module.i, "\n.profileImage[data-v-7037a547] {\n    width: 40px;\n}\nh4[data-v-7037a547] {\n  margin-bottom: 0px;\n  margin-left: 16px;\n}\n.match-options span[data-v-7037a547] {\n  margin: 5px;\n}\n.table-card[data-v-7037a547] {\n  padding: 20px;\n}\n.table th[data-v-7037a547] {\n  border-top: 0px;\n}\n.table tbody td[data-v-7037a547] {\n  color: rgb(99, 98, 98);\n  font-size: 14px;\n}\n.changePageSection div[data-v-7037a547] {\n  margin-right: 5px;\n}\n.form-row[data-v-7037a547] {\n  display: flex;\n  flex-direction: row;\n  justify-content: flex-start;\n}\n.form-row div[data-v-7037a547] {\n  margin-right: 5px;\n}\n.changePageSection[data-v-7037a547] {\n  margin-top: 10px;\n  display: flex;\n  justify-content: space-between;\n}\n.changePageSection div[data-v-7037a547]:nth-child(3) {\n  flex:1;\n}\n.countSelect[data-v-7037a547] {\n\n  margin-left: 5px;\n}\n\n", ""]);
+exports.push([module.i, "\n.profileImage[data-v-7037a547] {\n    width: 40px;\n}\nh4[data-v-7037a547] {\n  margin-bottom: 0px;\n  margin-left: 16px;\n}\n.match-options span[data-v-7037a547] {\n  margin: 5px;\n}\n.table-card[data-v-7037a547] {\n  padding: 20px;\n}\n.table th[data-v-7037a547] {\n  border-top: 0px;\n}\n.table tbody td[data-v-7037a547] {\n  color: rgb(99, 98, 98);\n  font-size: 14px;\n}\n.changePageSection div[data-v-7037a547] {\n  margin-right: 5px;\n}\n.form-row[data-v-7037a547] {\n  display: -webkit-flex;\n  display: flex;\n  -webkit-flex-direction: row;\n          flex-direction: row;\n  -webkit-justify-content: flex-start;\n          justify-content: flex-start;\n}\n.form-row div[data-v-7037a547] {\n  margin-right: 5px;\n}\n.changePageSection[data-v-7037a547] {\n  margin-top: 10px;\n  display: -webkit-flex;\n  display: flex;\n  -webkit-justify-content: space-between;\n          justify-content: space-between;\n}\n.changePageSection div[data-v-7037a547]:nth-child(3) {\n  -webkit-flex:1;\n          flex:1;\n}\n.countSelect[data-v-7037a547] {\n\n  margin-left: 5px;\n}\n\n", ""]);
 
 // exports
 
@@ -6906,19 +6840,9 @@ function toComment(sourceMap) {
  * @license  MIT
  */
 
-// The _isBuffer check is for Safari 5-7 support, because it's missing
-// Object.prototype.constructor. Remove this eventually
-module.exports = function (obj) {
-  return obj != null && (isBuffer(obj) || isSlowBuffer(obj) || !!obj._isBuffer)
-}
-
-function isBuffer (obj) {
-  return !!obj.constructor && typeof obj.constructor.isBuffer === 'function' && obj.constructor.isBuffer(obj)
-}
-
-// For Node v0.10 support. Remove this eventually.
-function isSlowBuffer (obj) {
-  return typeof obj.readFloatLE === 'function' && typeof obj.slice === 'function' && isBuffer(obj.slice(0, 0))
+module.exports = function isBuffer (obj) {
+  return obj != null && obj.constructor != null &&
+    typeof obj.constructor.isBuffer === 'function' && obj.constructor.isBuffer(obj)
 }
 
 
@@ -51692,8 +51616,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\search-filter\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\search-filter\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Wayne Cook\Desktop\filter_clone\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Wayne Cook\Desktop\filter_clone\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
